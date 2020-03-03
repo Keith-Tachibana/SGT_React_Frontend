63567d89a67c4b5201eadbf3f797518f1a2f39c2
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-03-02 17:25:26 -0800

    SGT React: View all grades with changes requested resolved | Keith Tachibana

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -1,11 +1,9 @@
-/* eslint-disable no-console */
 import React, { Component } from 'react';
 
 class Grade extends Component {
 
   render() {
     const { grades } = this.props;
-    console.log('Grades:', grades);
     return (
       <React.Fragment>
         {grades.map(grade => {
